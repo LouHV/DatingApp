6cb29d6eaf761c4a7662431ee7184185ca0ef037
LouHV <lviet13zx@gmail.com>
2023-07-28 17:04:17 +0700

    end ss6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,31 +1,36 @@
+import { Router } from '@angular/router';
+import { AccountService } from './../_services/account.service';
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
 import { User } from '../_models/user';
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
-  model : any = {}
+export class NavComponent implements OnInit{
+  model: any = {};
 
+  constructor(public accountService:AccountService,private router: Router,private toastr:ToastrService){ }
 
-  constructor(public accountService : AccountService){}
   ngOnInit(): void {
 
   }
-  login(){
-    this.accountService.login(this.model).subscribe(response=>{
-      console.log(response);
 
+  login(){
+    this.accountService.login(this.model).subscribe(response =>{
+      this.router.navigateByUrl('/members');
     },error =>{
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
+
   logout(){
     this.accountService.logout();
-
+    this.router.navigateByUrl('/');
   }
+
 }
